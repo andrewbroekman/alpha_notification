5e5c5592aa9ce090259ef59552a0b9ce51886d49
ThembaMbhele <mbhelethemba4@gmail.com>
2016-04-07 22:22:33 +0200

    Added the needed fields in the ReminderRequest class

--- src/main/java/com/codinginfinity/research/notification/ReminderRequest.java
+++ src/main/java/com/codinginfinity/research/notification/ReminderRequest.java
@@ -1,8 +1,11 @@
 package com.codinginfinity.research.notification;
 
 /**
- * Created by Avinash on 2016-04-07 project notification.
+ * @author Avinash Singh
+ * @author Themba Mbhele
  */
 public class ReminderRequest implements NotificationRequest
 {
+    User user;
+    String message;
 }
