f64182732d48e82fb805efb6b9cd1ef4d0acf8b8
GerardVanWyk <u14101263@tuks.co.za>
2016-04-07 22:24:45 +0200

    Added comments

--- src/main/java/com/codinginfinity/research/notification/ModificationRequest.java
+++ src/main/java/com/codinginfinity/research/notification/ModificationRequest.java
@@ -2,6 +2,9 @@ package com.codinginfinity.research.notification;
 
 /**
  * Created by Avinash on 2016-04-07 project notification.
+ * @param ModifyReminder a request object with the goal of modifying an existing reminder.
+ * @param ModifyReport a request object with the goal of modifying an existing report.
+ * @param ModifyBroadcast a request object with the goal of modifying an existing broadcast.
  */
 public interface ModificationRequest 
 {
