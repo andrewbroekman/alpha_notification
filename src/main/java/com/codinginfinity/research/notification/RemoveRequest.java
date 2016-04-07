43bc4a7dd7e9ba515f27bc18cf09b13513680465
GerardVanWyk <u14101263@tuks.co.za>
2016-04-07 22:33:52 +0200

    Added comments

--- src/main/java/com/codinginfinity/research/notification/RemoveRequest.java
+++ src/main/java/com/codinginfinity/research/notification/RemoveRequest.java
@@ -2,6 +2,9 @@ package com.codinginfinity.research.notification;
 
 /**
  * Created by Avinash on 2016-04-07 project notification.
+ * @param removeReminder request to remove an instance of reminder.
+ * @param removeReport request to remove an instance of report.
+ * @param removeBroadcast request to remove an instance of broadcast.
  */
 public interface RemoveRequest
 {
