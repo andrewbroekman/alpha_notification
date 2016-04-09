5524a624a7543ff31882478d1bcb7b7526cfba19
ThembaMbhele <mbhelethemba4@gmail.com>
2016-04-09 20:10:15 +0200

    Added functionality to the class

--- src/main/java/com/codinginfinity/research/notification/requests/NotificationRequest.java
+++ src/main/java/com/codinginfinity/research/notification/requests/NotificationRequest.java
@@ -1,11 +1,24 @@
 package com.codinginfinity.research.notification.requests;
 
+import com.codinginfinity.research.notification.Interval;
+import com.codinginfinity.research.notification.responses.NotificationResponse;
+
+import java.util.Date;
+import java.lang.annotation.*;
+
 /**
  * This interface is a wrapper for the different types of requests
- * @author Avinash
+ * @author COS301 - Alpha Notifications
  *
  */
 public interface NotificationRequest
 {
-    public void startSchedule();
+
+    public User getUser();
+    public String getMessage();
+    public Date getStartDate();
+    public Date getEndDate();
+    public Interval getInterval();
+
+
 }
