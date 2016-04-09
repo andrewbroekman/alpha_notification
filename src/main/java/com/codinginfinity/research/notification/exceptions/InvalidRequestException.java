e5139bc918ebef01d6e7fedcdf42888664902f66
ThembaMbhele <mbhelethemba4@gmail.com>
2016-04-09 20:03:42 +0200

    Added the InvalidRequestException class

---
+++ src/main/java/com/codinginfinity/research/notification/exceptions/InvalidRequestException.java
@@ -0,0 +1,28 @@
+package com.codinginfinity.research.notification.exceptions;
+
+/**
+ * @author COS301 - Alpha Notifications
+ */
+public class InvalidRequestException extends Exception
+{
+    public InvalidRequestException()
+    {
+        super();
+    }
+
+    public InvalidRequestException(String msg, Throwable source)
+    {
+        super(msg, source);
+    }
+
+    public InvalidRequestException(String msg)
+    {
+        super(msg);
+    }
+
+    public InvalidRequestException(Throwable source)
+    {
+        super(source);
+    }
+
+}
