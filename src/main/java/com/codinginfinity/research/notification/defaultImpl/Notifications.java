b43f17727dd6ad2811e2ec42c6e9f612bf299c6a
Avinash <u14043778@tuks.co.za>
2016-04-10 14:01:39 +0200

    updated the Notifications

--- src/main/java/com/codinginfinity/research/notification/defaultImpl/Notifications.java
+++ src/main/java/com/codinginfinity/research/notification/defaultImpl/Notifications.java
@@ -1,21 +1,19 @@
 package com.codinginfinity.research.notification.defaultImpl;
 
 import com.codinginfinity.research.notification.INotification;
-import com.codinginfinity.research.notification.Schedule;
 import com.codinginfinity.research.notification.exceptions.InvalidRequestException;
-import com.codinginfinity.research.notification.requests.ModifyNotificationRequest;
 import com.codinginfinity.research.notification.requests.NotificationRequest;
-import com.codinginfinity.research.notification.requests.RemoveRequest;
-import com.codinginfinity.research.notification.responses.ModificationResponse;
 import com.codinginfinity.research.notification.responses.NotificationResponse;
-import com.codinginfinity.research.notification.responses.RemoveResponse;
+import com.codinginfinity.research.notification.defaultImpl.Schedule;
+
+import javax.inject.Inject;
 
 /**
  * @author COS301 - Alpha Notifications
  */
 public class Notifications implements INotification
 {
-    @Inject
+
     private Schedule schedule;
 
     @Inject public NotificationResponse ActivitiesNotification(NotificationRequest request) throws InvalidRequestException
@@ -29,19 +27,22 @@ public class Notifications implements INotification
     @Inject public NotificationResponse ReportNotification(NotificationRequest request)throws InvalidRequestException
     {
         if (request == null) throw new InvalidRequestException("ReportNotificationRequest is NULL");
-        return request.start();
+        schedule = new Schedule(request);
+        return schedule.sendReportNotification();
     }
 
     @Inject public NotificationResponse ReminderNotification(NotificationRequest request)throws InvalidRequestException
     {
         if (request == null) throw new InvalidRequestException("ReminderNotificationRequest is NULL");
-        return request.start();
+        schedule = new Schedule(request);
+        return schedule.sendReminderNotification();
     }
 
     @Inject public NotificationResponse BroadcastNotification(NotificationRequest request)throws InvalidRequestException
     {
         if (request == null) throw new InvalidRequestException("BroadcastNotificationRequest is NULL");
-        return request.start();
+        schedule = new Schedule(request);
+        return schedule.sendBroadcastNotification();
     }
 
 //    @Inject public ModificationResponse ModifyNotification(ModifyNotificationRequest modify)throws InvalidRequestException
