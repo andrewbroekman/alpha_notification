4bc28b34fb78fb1e30a82b54d9d1b18c4f7602ad
Avinash <u14043778@tuks.co.za>
2016-04-10 14:06:23 +0200

    updated the Emailer to send emails

--- src/main/java/com/codinginfinity/research/notification/defaultImpl/Emailer.java
+++ src/main/java/com/codinginfinity/research/notification/defaultImpl/Emailer.java
@@ -1,50 +1,55 @@
-package com.codinginfinity.research.notification;
+package com.codinginfinity.research.notification.defaultImpl;
+
+import com.codinginfinity.research.notification.IEmailer;
+import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+import java.awt.*;
 import java.util.*;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Address;
 import javax.mail.*;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+
 import javax.mail.internet.*;
-import javax.activation.*;
+import javax.mail.Transport;
+import java.util.Properties;
 
 /**
  * Implementation of the IEmailer module which is used to send an email to a user.
  * Makes use of JavaMail API
  *
- * @author	Amy Lochner
- * @author	Christiaan Nel
- * @since 	2016-03-29
- *
- *	29/03/2016 Amy Lochner - worked on functionality and structure
- * 	29/03/2016 Christiaan Nel - worked on functionality and structure
- *
+ * @author	COS301 - Alpha Notifications
  */
+
 public class Emailer implements IEmailer
 {
 	/**
 	*	The email address of the sender.
 	*/
-	private static final String sender;
+	private String sender;
 	/**
 	*	The properties of the environment.
 	*/
-	private static final Properties properties;
+	protected Properties properties;
 	/**
 	*	A mail session that provides access to the protocol providers that implement the Store, Transport, and related classes.
 	*/
-	private static final Session session;
+	protected Session session;
+
+	protected Message msg;
 
 	/**
 	 * Utilised when initialising an instance of the Emailer class
-	 *
-	 * @param 	 sender - the email address from which the email must be sent
-	 * @param 	 host - the host on which the program runs    ----------->unsure
-	 * @param 	 mailServer - the server from which the mail is sent
-	 * 
+
 	 */
-	public Emailer(String sender ,String host, String mailServer)
+	public Emailer()
 	{
-		this.sender = sender;
-		this.properties = System.getProperties();
-		properties.setProperty(mailServer,host);
-		this.session = Session.getDefaultInstance(properties);
+
 	} 
 	/**
 	 * Utilised when called through the implementation of the Notifications interface to send a notification
@@ -59,11 +64,46 @@ public class Emailer implements IEmailer
 	{
 		try
 		{
-			sendMail(recipient,subject,message,null);
+			msg = new MimeMessage(session);
+			msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+			msg.addHeader("format", "flowed");
+			msg.addHeader("Content-Transfer-Encoding", "8bit");
+			msg.setFrom(new InternetAddress("smtp.gmail.com")); // @TODO add a host
+
+			properties = System.getProperties();
+			properties.put("mail.smtp.starttls.enable", "true");
+			properties.put("mail.smtp.starttls.required", "true");
+			properties.put("mail.smtp.auth", "true");
+			properties.setProperty("mail.smtp.host", "mail.testaplha301@gmail.com");
+
+			Authenticator auth = new Authenticator()
+			{
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication()
+				{
+					return super.getPasswordAuthentication("mail.testaplha301@gmail.com", "12345test");
+				}
+			};
+
+			session = Session.getDefaultInstance(properties, auth);
+
+			InternetAddress address = new InternetAddress(recipient);
+			msg.addRecipient(Message.RecipientType.TO, address);
+			msg.setSubject(subject);
+			msg.setSentDate(new Date());
+			Multipart mp = new MimeMultipart();
+
+			BodyPart bp = new MimeBodyPart();
+			bp.setText(message);
+
+			mp.addBodyPart(bp);
+			msg.setContent(mp);
+			Transport.send(msg);
 		}
-		catch(MessagingException exception)
+
+		catch (javax.mail.MessagingException e)
 		{
-			exception.printStackTrace();
+			e.printStackTrace();
 		}
 	}
 	/**
@@ -82,9 +122,9 @@ public class Emailer implements IEmailer
 		{
 
 			MimeMessage email = new MimeMessage(session);
-			message.setFrom(new InternetAddress(sender));
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
-			message.setSubject(subject);
+			email.setFrom(new InternetAddress(sender));
+			email.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+			email.setSubject(subject);
 
 			BodyPart messageBodyPart = new MimeBodyPart();
 			messageBodyPart.setText(message);
@@ -98,12 +138,62 @@ public class Emailer implements IEmailer
 				messageBodyPart = new MimeBodyPart();
 				//Incomplete
 			}
-			message.setContent(multipart);
-			Transport.send(message);
+			email.setContent(multipart);
+			Transport.send(email);
 		}
-		catch(MessagingException exception)
+		catch (AddressException ae)
 		{
-			exception.printStackTrace();
+			ae.printStackTrace();
+			return false;
 		}
+
+		catch (javax.mail.MessagingException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+
+	}
+
+	public boolean sendMail(ArrayList<String> recipient, String subject, String message) throws MessagingException
+	{
+		try
+		{
+
+			MimeMessage email = new MimeMessage(session);
+			email.setFrom(new InternetAddress(sender));
+			for (int i = 0; i < recipient.size(); ++i)
+			{
+				String s = recipient.get(i);
+
+				if (!s.equals(""))
+					email.addRecipient(Message.RecipientType.TO, new InternetAddress(s));
+			}
+
+			email.setSubject(subject);
+
+			BodyPart messageBodyPart = new MimeBodyPart();
+			messageBodyPart.setText(message);
+
+			Multipart multipart = new MimeMultipart();
+			multipart.addBodyPart(messageBodyPart);
+
+			email.setContent(multipart);
+			Transport.send(email);
+		}
+
+		catch (AddressException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+
+		catch (javax.mail.MessagingException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 }
