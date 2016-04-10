79789f8e5c9a90865ea8830ec59fe615335e66be
LochnerAmy <u14038600@tuks.co.za>
2016-04-10 14:55:47 +0200

    Updated

--- src/main/java/com/codinginfinity/research/notification/defaultImpl/Interval.java
+++ src/main/java/com/codinginfinity/research/notification/defaultImpl/Interval.java
@@ -1,15 +1,22 @@
 package com.codinginfinity.research.notification.defaultImpl;
 
 /**
+ *	Interval class which allows a user to specify a repeat interval
  * @author COS301 Team Alpha Notification
  */
 public class Interval
 {
+	/**
+	*	Duration unit options
+	*/
 	public enum DurationUnit
 	{
-	    days, weeks,months, years
+	    days,weeks,months,years
 	}
-	
+	/**
+	*	The interval specified by the user
+	*/
+	@Inject
 	private String interval;
 	
 	public Interval(String interval)
@@ -17,10 +24,17 @@ public class Interval
 		this.interval = interval.toLowerCase();
     }
 	
+	/**
+	*	Utilised to change the interval unit
+	*	@param interval - the new interval to unit
+	*/
 	public void setDuration(String interval)
 	{
 		this.interval = interval;
 	}
+	/**
+	*	Utilised to obtain the interval unit
+	*/
 	public String getDuration()
 	{
 		return this.interval;
