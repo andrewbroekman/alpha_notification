2fbb0507a5725a16b0e6c392bbbf15c38acafe6e
Themba Mbhele <themba@linuxmint.linuxmint-domain>
2016-04-14 11:53:17 +0200

    added unit tests

---
+++ src/test/java/com/codinginfinity/research/notification/UnitTest.java
@@ -0,0 +1,241 @@
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * @author COS301 Alpha Notification
+ */
+public class UnitTest
+{
+    private Notifications notification;
+    private NotificationRequest activitiesNotificationRequest;
+    private BroadcastNotificationRequest broadcastNotificationRequest;
+    private ReminderRequest reminderRequest;
+    private ReportNotificationRequest reportNotificationRequest;
+    private  NotificationResponse notificationResponse =null;
+
+    @Before
+    public void initializeObjects()
+    {
+        activitiesNotificationRequest = new ActivitiesNotificationRequest("1", new User("Themba", "Mbhele", "mbhelethemba4@gmail.com"),"This is the message to be sent!",new Date(), new Date(), new Interval(""));
+        notification = new Notifications();
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("mbhelethemba4@gmail.com");
+        list.add("lochnerAmy@gmail.com");
+        list.add("tashan.avi@gmail.com");
+        list.add("nelChristiaan@hotmail.com");
+        list.add("dregrar@gmail.com");
+        broadcastNotificationRequest = new BroadcastNotificationRequest("2",list,"This is a broadcast message",new Date(),new Date(), new Interval("weeks"));
+        reminderRequest = new ReminderRequest("3",new User("Amy","Lochner","lochnerAmy@gmail.com"),"This is a reminder: Your publication is due on Friday 22 April 2016", new Date(), new Date(),new Interval("days"));
+        reportNotificationRequest = new ReportNotificationRequest("4", new User("Avinash", "Singh", "tashan.avi@gmail.com"), "This is a report", "Vector-based_example.svg", new Date(), new Date(), new Interval("weekly"));
+
+    }
+
+//    @Test
+//    public void testReportNotification()
+//    {
+//
+//        try
+//        {
+//            notificationResponse = notification.ReportNotification(reportNotificationRequest);
+//            assertEquals("The code of the response object should be 'SUCCESS' ", "SUCCESS", notificationResponse.getCode());
+//        }
+//        catch(InvalidRequestException error)
+//        {
+//            System.out.println(error.getMessage());
+//        }
+//    }
+//
+//    @Test
+//    public void testReportNotificationEmailException()
+//    {
+//
+//        try
+//        {
+//            notificationResponse = notification.ReportNotification(null);
+//            assertEquals("The code of the response object should be 'FAILED' ", "FAILED", notificationResponse.getCode());
+//        }
+//        catch(InvalidRequestException error)
+//        {
+//            System.out.println(error.getMessage());
+//        }
+//    }
+
+    @Test
+    public void testActivityNotification()
+    {
+
+        try
+        {
+           notificationResponse = notification.ActivitiesNotification(activitiesNotificationRequest);
+            assertEquals("The code of the response object should be 'SUCCESS' ", "SUCCESS", notificationResponse.getCode());
+        }
+        catch(InvalidRequestException error)
+        {
+            System.out.println(error.getMessage());
+        }
+    }
+
+    @Test
+    public void testActivityNotificationNoMessage()
+    {
+
+        try
+        {
+            notificationResponse = notification.ActivitiesNotification(activitiesNotificationRequest = new ActivitiesNotificationRequest("1", new User("Themba", "Mbhele", "mbhelethemba4@gmail.com"),"",new Date(), new Date(), new Interval("")));
+            assertEquals("The code of the response object should be 'FAILED' ", "FAILED", notificationResponse.getCode());
+        }
+        catch(InvalidRequestException error)
+        {
+            System.out.println(error.getMessage());
+        }
+    }
+
+    @Test
+    public void testActivityNotificationNoUser()
+    {
+
+        try
+        {
+            notificationResponse = notification.ActivitiesNotification(activitiesNotificationRequest = new ActivitiesNotificationRequest("1", null,"",new Date(), new Date(), new Interval("")));
+            assertEquals("The code of the response object should be 'FAILED' ", "FAILED", notificationResponse.getCode());
+        }
+        catch(InvalidRequestException error)
+        {
+            System.out.println(error.getMessage());
+        }
+    }
+
+    @Test
+    public void testActivityNotificationNoRecipient()
+    {
+
+        try
+        {
+            notificationResponse = notification.ActivitiesNotification(activitiesNotificationRequest = new ActivitiesNotificationRequest("1", new User("Themba", "Mbhele", ""),"",new Date(), new Date(), new Interval("")));
+            assertEquals("The code of the response object should be 'FAILED' ", "FAILED", notificationResponse.getCode());
+        }
+        catch(InvalidRequestException error)
+        {
+            System.out.println(error.getMessage());
+        }
+    }
+
+    @Test
+    public void testActivityNotificationEmailException()
+    {
+
+        try
+        {
+            notificationResponse = notification.ActivitiesNotification(null);
+            assertEquals("The code of the response object should be 'FAILED' ", "FAILED", notificationResponse.getCode());
+        }
+        catch(InvalidRequestException error)
+        {
+            System.out.println(error.getMessage());
+        }
+    }
+    @Test
+    public void testBroadcastNotification()
+    {
+        try
+        {
+            notificationResponse = notification.BroadcastNotification(broadcastNotificationRequest);
+            assertEquals("The code of the response object should be 'SUCCESS' ", "SUCCESS", notificationResponse.getCode());
+        }
+        catch(InvalidRequestException error)
+        {
+            System.out.println(error.getMessage());
+        }
+    }
+
+    @Test
+    public void testBroadcastNotificationNoUsers()
+    {
+        try
+        {
+            notificationResponse = notification.BroadcastNotification(broadcastNotificationRequest = new BroadcastNotificationRequest("2",null,"This is a broadcast message",null,new Date(), new Interval("weeks")));
+            assertEquals("The code of the response object should be 'FAILED' ", "FAILED", notificationResponse.getCode());
+        }
+        catch(InvalidRequestException error)
+        {
+            System.out.println(error.getMessage());
+        }
+    }
+
+    @Test
+    public void testBroadcastNotificationNoStartDate()
+    {
+
+
+        try
+        {
+            ArrayList<String> list = new ArrayList<String>();
+            list.add("mbhelethemba4@gmail.com");
+            list.add("lochnerAmy@gmail");
+            list.add("tashan.avi@gmail.com");
+            list.add("nelChristiaan@hotmail.com");
+            list.add("dregrar@gmail.com");
+            notificationResponse = notification.BroadcastNotification(new BroadcastNotificationRequest("2",list,"This is a broadcast message",null,new Date(), new Interval("weeks")));
+            assertEquals("The code of the response object should be 'FAILED' ", "FAILED", notificationResponse.getCode());
+        }
+        catch(InvalidRequestException error)
+        {
+            System.out.println(error.getMessage());
+        }
+    }
+    @Test
+    public void testBroadcastNotificationEmailException()
+    {
+        try
+        {
+            notificationResponse = notification.BroadcastNotification(null);
+            assertEquals("The code of the response object should be 'FAILED' ", "FAILED", notificationResponse.getCode());
+        }
+        catch(InvalidRequestException error)
+        {
+            System.out.println(error.getMessage());
+        }
+    }
+
+    @Test
+    public void testRemindertNotification()
+    {
+        try
+        {
+            notificationResponse = notification.ReminderNotification(reminderRequest);
+            assertEquals("The code of the response object should be 'SUCCESS' ", "SUCCESS", notificationResponse.getCode());
+        }
+        catch(InvalidRequestException error)
+        {
+            System.out.println(error.getMessage());
+        }
+    }
+    @Test
+    public void testReminderNotificationEmailException()
+    {
+
+        try
+        {
+            notificationResponse = notification.ReminderNotification(null);
+            assertEquals("The code of the response object should be 'FAILED' ", "FAILED", notificationResponse.getCode());
+        }
+        catch(InvalidRequestException error)
+        {
+            System.out.println(error.getMessage());
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Result result = JUnitCore.runClasses(UnitTest.class);
+    }
+}
