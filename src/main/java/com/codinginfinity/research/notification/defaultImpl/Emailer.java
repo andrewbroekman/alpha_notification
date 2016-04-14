d289773e28f2f09ff7f333c681f6cb43a7c8eef0
Themba Mbhele <themba@linuxmint.linuxmint-domain>
2016-04-14 11:11:33 +0200

    further intergarion with mail interface

--- src/main/java/com/codinginfinity/research/notification/defaultImpl/Emailer.java
+++ src/main/java/com/codinginfinity/research/notification/defaultImpl/Emailer.java
@@ -1,12 +1,12 @@
-package com.codinginfinity.research.notification.defaultImpl;
+package com.codinginfinity.research.notification.defaultImp;
 
-import com.codinginfinity.research.notification.IEmailer;
-import com.codinginfinity.research.notification.exceptions.ImageException;
-import com.codinginfinity.research.notification.exceptions.RecipientException;
-
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.*;
 import java.awt.*;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Properties;
@@ -27,22 +27,22 @@ public class Emailer implements IEmailer
 	/**
 	 * The password of the sender.
 	 */
-	private String password;
+	protected String password;
 	/**
 	 * The properties of the environment.
 	 */
 
-	private Properties properties;
+	protected Properties properties;
 	/**
 	 * A mail session that provides access to the protocol providers that implement the Store, Transport, and related classes.
 	 */
-	private Session session;
+	protected Session session;
 	/**
 	 * The server that hosts the sender address
 	 */
-	private String host;
+	protected String host;
 
-	private Message msg;
+	protected Message msg;
 
 	public Emailer()
 	{
@@ -73,11 +73,11 @@ public class Emailer implements IEmailer
 	 * @param subject   - the subject line of the email
 	 * @param message   - the body of the email
 	 * @return True if the email was sent successfully
+	 * @throws throws a MessagingException
 	 */
 	public boolean sendMail(String recipient, String subject, String message)
 	{
-		try
-		{
+		try{
 			// Create a default MimeMessage object.
 			MimeMessage msg = new MimeMessage(session);
 
@@ -120,61 +120,92 @@ public class Emailer implements IEmailer
 	 * @param message   - the body of the email
 	 * @param image     - the image of the report to be sent to a user
 	 * @return True if the email was sent successfully
+	 * @throws throws a MessagingException
 	 */
 	public boolean sendMail(String recipient, String subject, String message, Image image)
 	{
-		try
-		{
-			msg = new MimeMessage(session);
-			msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
-			msg.addHeader("format", "flowed");
-			msg.addHeader("Content-Transfer-Encoding", "8bit");
-			msg.setFrom(new InternetAddress("smtp.gmail.com")); // @TODO add a host
-
-			properties = System.getProperties();
-			properties.put("mail.smtp.starttls.enable", "true");
-			properties.put("mail.smtp.starttls.required", "true");
-			properties.put("mail.smtp.auth", "true");
-			properties.setProperty("mail.smtp.host", "mail.testaplha301@gmail.com");
-
-
-			InternetAddress address = new InternetAddress(recipient);
-			msg.addRecipient(Message.RecipientType.TO, address);
-			msg.setSubject(subject);
-			msg.setSentDate(new Date());
-			Multipart mp = new MimeMultipart();
-
-			BodyPart bp = new MimeBodyPart();
-			bp.setText(message);
-
+//		try
+//		{
 //
-//			bp = new MimeBodyPart();
-//			DataSource source = new FileDataSource();
-//			bp.setDataHandler(new DataHandler(source));
-//			bp.setFileName();
+//			InternetAddress address = new InternetAddress(recipient);
+//			msg.addRecipient(Message.RecipientType.TO, address);
+//			msg.setSubject(subject);
+//			msg.setSentDate(new Date());
+//			Multipart mp = new MimeMultipart();
+//
+//			BodyPart bp = new MimeBodyPart();
+//			bp.setText(message);
+//
+////
+////			bp = new MimeBodyPart();
+////			DataSource source = new FileDataSource();
+////			bp.setDataHandler(new DataHandler(source));
+////			bp.setFileName();
+////			mp.addBodyPart(bp);
+//
+//
+//
+//
+//
 //			mp.addBodyPart(bp);
-
-
-
-
-
-			mp.addBodyPart(bp);
-			msg.setContent(mp);
-			Transport.send(msg);
-		}
-		catch (javax.mail.MessagingException e)
-		{
-			e.printStackTrace();
-			return false;
-		}
+//			msg.setContent(mp);
+//			Transport.send(msg);
+//		}
+//		catch (javax.mail.MessagingException e)
+//		{
+//			e.printStackTrace();
+//			return false;
+//		}
 
 		return true;
 	}
 
-
 	public boolean sendMail(String recipient, String subject, String message, String imagePath) throws MessagingException, RecipientException, ImageException
 	{
-		return false;
+		try{
+			// Create a default MimeMessage object.
+			msg = new MimeMessage(session);
+
+			// Set From: header field of the header.
+
+			// Set To: header field of the header.
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+
+			// Set Subject: header field
+			msg.setSubject("Testing Subject");
+
+			// Create the message part
+			BodyPart messageBodyPart = new MimeBodyPart();
+
+			// Now set the actual message
+			messageBodyPart.setText("This is message body");
+
+			// Create a multipar message
+			Multipart multipart = new MimeMultipart();
+
+			// Set text message part
+			multipart.addBodyPart(messageBodyPart);
+
+			// Part two is attachment
+			messageBodyPart = new MimeBodyPart();
+			String filename = imagePath;
+			DataSource source = new FileDataSource(filename);
+			messageBodyPart.setDataHandler(new DataHandler(source));
+			messageBodyPart.setFileName(filename);
+			multipart.addBodyPart(messageBodyPart);
+
+			// Send the complete message parts
+			msg.setContent(multipart);
+
+			// Send message
+			Transport.send(msg);
+			//source.getOutputStream().close();
+
+			System.out.println("Sent message successfully....");
+		}catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+		return true;
 	}
 
 
@@ -187,7 +218,6 @@ public class Emailer implements IEmailer
 			msg.addHeader("format", "flowed");
 			msg.addHeader("Content-Transfer-Encoding", "8bit");
 			msg.setFrom(new InternetAddress("smtp.gmail.com")); // @TODO add a host
-
 
 			for (int i = 0; i < recipient.size(); ++i)
 			{
@@ -209,7 +239,7 @@ public class Emailer implements IEmailer
 
 			mp.addBodyPart(bp);
 			msg.setContent(mp);
-			//Transport.send(msg);
+			Transport.send(msg);
 		}
 
 		catch (javax.mail.MessagingException e)
